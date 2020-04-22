654f23c8516400aec77967b77142aac382f93273
Glitch (hello-webpage) <none>
2020-04-22 01:22:49 +0000

    🍘👛 Checkpoint ./tower_defense.html:9586634/11377 ./js/start_click.js:9586634/2070

--- js/start_click.js
+++ js/start_click.js
@@ -2,6 +2,16 @@ AFRAME.registerComponent("cursor-listener", {
   init: function() {
     this.el.addEventListener("click", function(evt) {
       console.log("I was clicked at:");
+      document
+        .querySelector("#start")
+        .parentNode.removeChild(document.querySelector("#start"));
+      document
+        .querySelector("#text1")
+        .parentNode.removeChild(document.querySelector("#text1"));
+      document
+        .querySelector("#text2")
+        .parentNode.removeChild(document.querySelector("#text2"));
+
       start();
     });
   }
