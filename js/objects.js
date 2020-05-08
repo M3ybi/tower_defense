a40456aa9f57f233c01cbada4a7e37c96f6fd2a7
Glitch (hello-webpage) <none>
2020-05-08 15:49:12 +0000

    🎮🌮 Checkpoint ./js/objects.js:9586634/348 ./tower_defense.html:9586634/985

--- js/objects.js
+++ js/objects.js
@@ -1,5 +1,6 @@
+var objects = Array("drone-red-obj1","drone-green-obj1","drone-red-obj2","drone-green-obj2","drone-red-obj2","drone-green-obj2");
 function generate_object (){
-  var objects = Array("drone-red-obj1","drone-green-obj1","drone-red-obj2","drone-green-obj2","drone-red-obj2","drone-green-obj2");
+  var item = objects[Math.floor(Math.random() * objects.length)];
 }
 
 function connect_mtl(){
